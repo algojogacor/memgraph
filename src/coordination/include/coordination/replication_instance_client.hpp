0f61aa01105fbc96c4d9795d0616e1c60d1b35ee
as51340 <andi8647@gmail.com>
2026-05-13 16:10:13 +0200

    feat: Coord->data and coord->coord configuration

--- src/coordination/include/coordination/replication_instance_client.hpp
+++ src/coordination/include/coordination/replication_instance_client.hpp
@@ -21,6 +21,7 @@
 #include "utils/event_counter.hpp"
 #include "utils/metrics_timer.hpp"
 #include "utils/scheduler.hpp"
+#include "utils/tls.hpp"
 
 namespace memgraph::metrics {
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
@@ -57,7 +58,8 @@ class ReplicationInstanceClient {
  public:
   explicit ReplicationInstanceClient(std::string instance_name, io::network::Endpoint mgt_server,
                                      CoordinatorInstance *coord_instance,
-                                     std::chrono::seconds instance_health_check_frequency_sec);
+                                     std::chrono::seconds instance_health_check_frequency_sec,
+                                     std::optional<utils::TlsConfig> const &tls_config);
 
   ~ReplicationInstanceClient() = default;
 
